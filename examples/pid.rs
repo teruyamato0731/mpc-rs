dd9f5eb1f5660d786214f75f43b9542af60d4209
teruyamato0731 <teruyamato0731@gmail.com>
2024-10-19 09:21:59 +0000

    feat: PIDでも動かしてみる

---
+++ examples/pid.rs
@@ -0,0 +1,78 @@
+extern crate nalgebra as na;
+use advanced_pid::{PidConfig, PidController, VelPid};
+use na::{vector, Vector4};
+use std::f64::consts::PI;
+
+const DT: f64 = 1e-3;
+
+fn main() {
+    let mut x: Vector4<f64> = vector![-0.5, 0.0, 0.2, 0.0];
+
+    // ログファイルの作成
+    let file_path = "logs/pid.csv";
+    let mut wtr = csv::Writer::from_path(file_path).expect("file open error");
+
+    let mut pid_theta = VelPid::new(PidConfig::new(6e-1, 4e-1, 5e-3).with_limits(-25.0, 25.0));
+    // let mut pid_theta = VelPid::new(PidConfig::new(5.2e-1, 6e-1, 5e-5).with_limits(-25.0, 25.0));
+
+    let now = std::time::Instant::now();
+    let mut i = 0;
+    while i as f64 * DT < 10.0 {
+        let t = i as f64 * DT;
+        let p = 0.5;
+        let phase = x[0].clamp(-p, p) * PI / p / 2.0;
+        let theta_ref = -0.2 * phase.sin().powi(5);
+        // let phase = x[0].clamp(-p, p) * PI / p;
+        // let theta_ref = -0.15 * (1.0 - phase.cos()) / 2.0 * phase.signum();
+        let u = -pid_theta.update(theta_ref, x[2], DT);
+        x = dynamics(&x, u);
+
+        // 0.1秒ごとにログを記録
+        if i % (0.1 / DT) as i32 == 0 {
+            println!(
+                "t: {:.2}, r: {:8.5}, u: {:8.3}, x: [{:10.4}, {:6.2}, {:5.2}, {:5.2}]",
+                t, theta_ref, u, x[0], x[1], x[2], x[3]
+            );
+
+            wtr.write_record(&[
+                t.to_string(),
+                u.to_string(),
+                theta_ref.to_string(),
+                x[0].to_string(),
+                x[1].to_string(),
+                x[2].to_string(),
+                x[3].to_string(),
+            ])
+            .expect("write error");
+            wtr.flush().expect("flush error");
+        }
+
+        // x[2] が 60度 以上になったら終了
+        if x[2].abs() > 60.0f64.to_radians() {
+            println!("x[2] is over 60 degrees");
+            break;
+        }
+
+        i += 1;
+    }
+    println!("elapsed: {:.2} sec", now.elapsed().as_secs_f64());
+}
+
+// 系ダイナミクスを記述
+const M1: f64 = 150e-3;
+const R_W: f64 = 50e-3;
+const M2: f64 = 2.3 - 2.0 * M1 + 2.0;
+const L: f64 = 0.2474; // 重心までの距離
+const J1: f64 = M1 * R_W * R_W;
+const J2: f64 = 0.2;
+const G: f64 = 9.81;
+const KT: f64 = 0.15; // m2006
+const D: f64 = (M1 + M2 + J1 / R_W * R_W) * (M2 * L * L + J2) - M2 * M2 * L * L;
+fn dynamics(state: &na::Vector4<f64>, u: f64) -> na::Vector4<f64> {
+    let mut x = *state;
+    x[3] += ((M1 + M2 + J1 / R_W * R_W) / D * M2 * G * L * x[2] - M2 * L / D / R_W * KT * u) * DT;
+    x[2] += x[3] * DT;
+    x[1] += (-M2 * M2 * G * L * L / D * x[2] + (M2 * L * L + J2) / D / R_W * KT * u) * DT;
+    x[0] += x[1] * DT;
+    x
+}
---
+++ scripts/plot-pid.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import matplotlib.patches as patches
+import datetime
+import shutil
+import os
+
+t_delta = datetime.timedelta(hours=9)
+JST = datetime.timezone(t_delta, 'JST')
+now = datetime.datetime.now(JST)
+d = now.strftime('%Y%m%d%H%M%S')
+r = 0.05
+L = 0.270
+W = 200e-3
+
+src = "logs/pid.csv"
+dst = f"logs/pid_{d}.csv"
+shutil.copy2(src, dst)
+
+data_set = np.loadtxt(
+    fname=dst, #読み込むファイルのパスと名前
+    dtype="float", #floatで読み込む
+    delimiter=",", #csvなのでカンマで区切る
+)
+
+DT = data_set[1, 0] - data_set[0, 0]
+
+# 1列目が time
+# 2列目が u
+# 3~7列目が x[0], x[1], x[2], x[3]
+
+fig, (ax, ax2) = plt.subplots(2, 1)
+fig.subplots_adjust(left=0.2, right=0.8)
+
+# 左軸 x, x', u
+# 右軸 θ, θ'
+
+ax2_2 = ax2.twinx()
+ax2.plot(data_set[:, 0], data_set[:, 1], label="u", linestyle='-.', color='r')
+ax2.plot(data_set[:, 0], data_set[:, 3], label="x", color='b')
+ax2.plot(data_set[:, 0], data_set[:, 4], label="x'", color='g')
+ax2_2.plot(data_set[:, 0], data_set[:, 2], label="r", color='c')
+ax2_2.plot(data_set[:, 0], data_set[:, 5], label="θ", linestyle='-.', color='gold')
+ax2_2.plot(data_set[:, 0], data_set[:, 6], label="θ'", linestyle='-.', color='darkorange')
+ax2.set_xlabel("time [s]")
+ax2.set_ylabel("displacement [m, m/s, 1]")
+ax2_2.set_ylabel("angle [rad, rad/s]")
+
+ax2.legend(loc='center right', bbox_to_anchor=(-0.1, 1))
+ax2_2.legend(loc='center left', bbox_to_anchor=(1.1, 1))
+
+# ax2に縦線を描画しアニメーションと一致させる
+line = ax2.axvline(x=0, color='k', linestyle=':')
+
+scale = 1
+xlim_max = data_set[:, 3].max() if data_set[:, 3].max() > scale else scale
+xlim_min = data_set[:, 3].min() if data_set[:, 3].min() < -scale else -scale
+ax.set_xlim(xlim_min-1, xlim_max+1)
+ax.set_ylim(-r, 0.5)
+
+c = patches.Circle(xy=(0, 0), radius=r, fc='None', ec='k')
+rect = patches.Rectangle(xy=(1, 1), width=W, height=W, angle=45, rotation_point='center', ec='k', fill=False)
+con = patches.ConnectionPatch((0, 0), (1, 1), coordsA='data', linewidth=3, ec='k', fc='w', label="act")
+ax.add_patch(c)
+ax.add_patch(con)
+ax.add_patch(rect)
+
+ax.legend()
+
+def update_anim(step, _step_max):
+    x = data_set[step, 3]
+    theta = data_set[step, 5]
+    X = x + L * np.sin(theta)
+    Y = L * np.cos(theta)
+    c.center = (x, 0)
+    rect.set_xy((X-W/2, Y-W/2))
+    rect.set_angle(-theta * 180 / np.pi)
+    con.xy1 = x, 0
+    con.xy2 = X, Y
+    line.set_xdata([data_set[step, 0], data_set[step, 0]])
+
+    # theta_est = data_set[step, 8]
+    # con_est.xy1 = data_set[step, 6], 0
+    # con_est.xy2 = data_set[step, 6] + L * np.sin(theta_est), L * np.cos(theta_est)
+
+    t = data_set[step, 0]
+    ax.set_title(f"step={step:4}, t={t:.3f}")
+
+    if step >= _step_max:
+        print("end")
+        plt.close()
+
+interval = DT * 1000
+
+ani = FuncAnimation(fig, update_anim, fargs=(len(data_set),), interval=interval, frames=len(data_set), repeat=False)
+
+path = f"imgs/anim_pid_{d}.mp4"
+s = f"<video src=\"../{path}\" controls autoplay loop></video>\n"
+
+try:
+    ani.save(path, writer="ffmpeg")
+    with open('imgs/anim.md', mode='r') as reader:
+        s = s + reader.read()
+    with open("imgs/anim.md", mode='w') as f:
+        f.write(s)
+    print(f"saved: {path}")
+except Exception as e:
+    print(e)
+    os.remove(path)
+
+"""
+xhost +local:
+"""
