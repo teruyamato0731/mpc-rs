c0f0cbd4d477d234d755580fe9c0916e6f2a5d98
teruyamato0731 <teruyamato0731@gmail.com>
2024-11-03 06:11:17 +0000

    refactor: 定数と型名を定義する

--- src/ukf.rs
+++ src/ukf.rs
@@ -1,29 +1,35 @@
+// 状態空間の次元
+const N: usize = 4;
+// 観測空間の次元
+const O: usize = 3;
+// シグマ点の数
+const M: usize = 2 * N + 1;
+
+type State = na::SVector<f64, N>;
+type Cov<const N: usize> = na::SMatrix<f64, N, N>;
+type Sigma<const S: usize> = na::SMatrix<f64, S, M>;
+
 pub struct UnscentedKalmanFilter {
-    x: na::Vector4<f64>,
-    p: na::Matrix4<f64>,
-    q: na::Matrix4<f64>,
-    r: na::Matrix3<f64>,
-    wm: na::SVector<f64, 9>,
-    wc: na::SVector<f64, 9>,
-    sigma_f: na::SMatrix<f64, 4, 9>,
+    x: State,
+    p: Cov<N>,
+    q: Cov<N>,
+    r: Cov<O>,
+    wm: Sigma<1>,
+    wc: Sigma<1>,
+    sigma_f: Sigma<N>,
 }
 
 impl UnscentedKalmanFilter {
-    const N: f64 = 4.0;
+    const N: f64 = N as f64;
     const ALPHA: f64 = 1e-3;
     const BETA: f64 = 2.0;
     const KAPPA: f64 = 3.0 - Self::N;
     const C: f64 = Self::ALPHA * Self::ALPHA * (Self::N + Self::KAPPA);
     const LAMBDA: f64 = Self::C - Self::N;
 
-    pub fn new(
-        x: na::Vector4<f64>,
-        p: na::Matrix4<f64>,
-        q: na::Matrix4<f64>,
-        r: na::Matrix3<f64>,
-    ) -> Self {
+    pub fn new(x: State, p: Cov<N>, q: Cov<N>, r: Cov<O>) -> Self {
         let (wm, wc) = Self::sigma_weight();
-        let sigma_f = Self::compute_sigma_points(x, p, 0.0, |x, _| *x);
+        let sigma_f = Sigma::<N>::from_element(f64::NAN);
         Self {
             x,
             p,
@@ -37,25 +43,25 @@ impl UnscentedKalmanFilter {
 
     pub fn predict<F>(&mut self, u: f64, fx: F)
     where
-        F: Fn(&na::Vector4<f64>, f64) -> na::Vector4<f64>,
+        F: Fn(&State, f64) -> State,
     {
-        self.sigma_f = Self::compute_sigma_points(self.x, self.p, u, fx);
+        self.sigma_f = Self::compute_sigma_points(&self.x, &self.p, u, fx);
         let (x, p) = Self::unscented_transform(&self.sigma_f, &self.wm, &self.wc, &self.q);
         self.x = x;
         self.p = p;
     }
 
-    pub fn update<F>(&mut self, x_obs: &na::Vector3<f64>, hx: F)
+    pub fn update<F>(&mut self, x_obs: &na::SVector<f64, O>, hx: F)
     where
-        F: Fn(&na::Vector4<f64>) -> na::Vector3<f64>,
+        F: Fn(&State) -> na::SVector<f64, O>,
     {
-        let mut sigmas_h = na::SMatrix::<f64, 3, 9>::zeros();
-        for i in 0..9 {
+        let mut sigmas_h = na::SMatrix::<f64, O, M>::zeros();
+        for i in 0..M {
             sigmas_h.set_column(i, &hx(&self.sigma_f.column(i).into_owned()));
         }
         let (zp, pz) = Self::unscented_transform(&sigmas_h, &self.wm, &self.wc, &self.r);
-        let mut pxz = na::SMatrix::<f64, 4, 3>::zeros();
-        for i in 0..9 {
+        let mut pxz = na::SMatrix::<f64, N, O>::zeros();
+        for i in 0..M {
             pxz += self.wc[i]
                 * (self.sigma_f.column(i) - self.x)
                 * (sigmas_h.column(i) - zp).transpose();
@@ -67,70 +73,61 @@ impl UnscentedKalmanFilter {
         self.p = (self.p + self.p.transpose()) / 2.0;
     }
 
-    fn compute_sigma_points<F>(
-        x: na::Vector4<f64>,
-        p: na::Matrix4<f64>,
-        u: f64,
-        fx: F,
-    ) -> na::SMatrix<f64, 4, 9>
+    fn compute_sigma_points<F>(x: &State, p: &Cov<N>, u: f64, fx: F) -> Sigma<N>
     where
-        F: Fn(&na::Vector4<f64>, f64) -> na::Vector4<f64>,
+        F: Fn(&State, f64) -> State,
     {
-        let mut sigma_f = Self::sigma_points(&x, &p);
-        for i in 0..9 {
+        let mut sigma_f = Self::sigma_points(x, p);
+        for i in 0..M {
             sigma_f.set_column(i, &fx(&sigma_f.column(i).into_owned(), u));
         }
         sigma_f
     }
 
     // 推定した状態を返す
-    pub fn state(&self) -> na::Vector4<f64> {
+    pub fn state(&self) -> State {
         self.x
     }
 
-    pub fn covariance(&self) -> na::Matrix4<f64> {
+    pub fn covariance(&self) -> Cov<N> {
         self.p
     }
 
     fn unscented_transform<const S: usize>(
-        sigmas: &na::SMatrix<f64, S, 9>,
-        wm: &na::SVector<f64, 9>,
-        wc: &na::SVector<f64, 9>,
-        cov: &na::SMatrix<f64, S, S>,
-    ) -> (na::SVector<f64, S>, na::SMatrix<f64, S, S>) {
-        let x = sigmas * wm;
-        let y = sigmas - na::SMatrix::<f64, S, 9>::from_columns(&[x, x, x, x, x, x, x, x, x]);
-        let mut tmp = na::SMatrix::<f64, S, S>::zeros();
-        for i in 0..9 {
+        sigmas: &Sigma<S>,
+        wm: &Sigma<1>,
+        wc: &Sigma<1>,
+        cov: &Cov<S>,
+    ) -> (na::SVector<f64, S>, Cov<S>) {
+        let x = sigmas * wm.transpose();
+        let y = sigmas - Sigma::<S>::from_columns(&[x; M]);
+        let mut tmp = Cov::<S>::zeros();
+        for i in 0..M {
             tmp += wc[i] * y.column(i) * y.column(i).transpose();
         }
         let p = tmp + cov;
         (x, p)
     }
 
-    fn sigma_weight() -> (na::SVector<f64, 9>, na::SVector<f64, 9>) {
-        let mut wm = na::SVector::<f64, 9>::from_element(1.0 / (2.0 * Self::C));
-        let mut wc = na::SVector::<f64, 9>::from_element(1.0 / (2.0 * Self::C));
+    fn sigma_weight() -> (Sigma<1>, Sigma<1>) {
+        let mut wm = Sigma::<1>::from_element(1.0 / (2.0 * Self::C));
+        let mut wc = Sigma::<1>::from_element(1.0 / (2.0 * Self::C));
         wm[0] = Self::LAMBDA / Self::C;
         wc[0] = Self::LAMBDA / Self::C + 1.0 - Self::ALPHA.powi(2) + Self::BETA;
         (wm, wc)
     }
 
-    fn sigma_points(x: &na::Vector4<f64>, p: &na::Matrix4<f64>) -> na::SMatrix<f64, 4, 9> {
+    fn sigma_points(x: &State, p: &Cov<N>) -> Sigma<N> {
         let svd = (Self::C * p).svd_unordered(true, false);
-        let s_sqrt = na::Matrix4::<f64>::from_diagonal(&svd.singular_values.map(|x| x.sqrt()));
+        let s_sqrt = Cov::<N>::from_diagonal(&svd.singular_values.map(|x| x.sqrt()));
         let u = svd.u.unwrap();
         let l = u * s_sqrt;
-        na::SMatrix::<f64, 4, 9>::from_columns(&[
-            *x,
-            *x + l.column(0),
-            *x - l.column(0),
-            *x + l.column(1),
-            *x - l.column(1),
-            *x + l.column(2),
-            *x - l.column(2),
-            *x + l.column(3),
-            *x - l.column(3),
-        ])
+        let mut sigma_points = Sigma::<N>::zeros();
+        sigma_points.set_column(0, x);
+        for i in 0..N {
+            sigma_points.set_column(1 + i, &(x + l.column(i)));
+            sigma_points.set_column(1 + N + i, &(x - l.column(i)));
+        }
+        sigma_points
     }
 }
