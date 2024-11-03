3c8e67edd5a34529d601166609a471b68d36d665
teruyamato0731 <teruyamato0731@gmail.com>
2024-11-03 14:45:06 +0000

    refactor: マクロで短縮する

--- src/packet.rs
+++ src/packet.rs
@@ -23,57 +23,36 @@ pub struct Sensor {
     pub gyro: f32,         // ジャイロセンサの角速度
 }
 
-impl State {
-    pub const SIZE: usize = std::mem::size_of::<Self>();
-    pub const BUF_SIZE: usize = Self::SIZE + 2;
+macro_rules! impl_cobs_convertible {
+    ($type:ty) => {
+        impl $type {
+            pub const SIZE: usize = std::mem::size_of::<Self>();
+            pub const BUF_SIZE: usize = Self::SIZE + 2;
 
-    // cobsバイト列に変換
-    pub fn as_cobs(&self) -> [u8; Self::BUF_SIZE] {
-        let buf: [u8; Self::SIZE] = self.as_bytes().try_into().unwrap();
-        cobs_rs::stuff(buf, 0)
-    }
-    // cobsバイト列から復元
-    pub fn from_cobs(buf: &[u8; Self::BUF_SIZE]) -> Option<Self> {
-        let (cobs, _): ([u8; Self::SIZE], _) = cobs_rs::unstuff(*buf, 0);
-        Self::read_from(&cobs)
-    }
+            // cobsバイト列に変換
+            pub fn as_cobs(&self) -> [u8; Self::BUF_SIZE] {
+                let buf: [u8; Self::SIZE] = self.as_bytes().try_into().unwrap();
+                cobs_rs::stuff(buf, 0)
+            }
+            // cobsバイト列から復元
+            pub fn from_cobs(buf: &[u8; Self::BUF_SIZE]) -> Option<Self> {
+                let (cobs, _): ([u8; Self::SIZE], _) = cobs_rs::unstuff(*buf, 0);
+                Self::read_from(&cobs)
+            }
+        }
+    };
 }
+
+impl_cobs_convertible!(State);
+impl_cobs_convertible!(Control);
+impl_cobs_convertible!(Sensor);
 
 impl Control {
     pub const MAX: i16 = 16000;
-    pub const SIZE: usize = std::mem::size_of::<Self>();
-    pub const BUF_SIZE: usize = Self::SIZE + 2;
-
-    // cobsバイト列に変換
-    pub fn as_cobs(&self) -> [u8; Self::BUF_SIZE] {
-        let buf: [u8; Self::SIZE] = self.as_bytes().try_into().unwrap();
-        cobs_rs::stuff(buf, 0)
-    }
-    // cobsバイト列から復元
-    pub fn from_cobs(buf: &[u8; Self::BUF_SIZE]) -> Option<Self> {
-        let (cobs, _): ([u8; Self::SIZE], _) = cobs_rs::unstuff(*buf, 0);
-        Self::read_from(&cobs)
-    }
     pub fn from_current(current: f64) -> Self {
         const K: f64 = Control::MAX as f64 / 20.0;
         let u = (K * current) as i16;
         Control { u }
-    }
-}
-
-impl Sensor {
-    pub const SIZE: usize = std::mem::size_of::<Self>();
-    pub const BUF_SIZE: usize = Self::SIZE + 2;
-
-    // cobsバイト列に変換
-    pub fn as_cobs(&self) -> [u8; Self::BUF_SIZE] {
-        let buf: [u8; Self::SIZE] = self.as_bytes().try_into().unwrap();
-        cobs_rs::stuff(buf, 0)
-    }
-    // cobsバイト列から復元
-    pub fn from_cobs(buf: &[u8; Self::BUF_SIZE]) -> Option<Self> {
-        let (cobs, _): ([u8; Self::SIZE], _) = cobs_rs::unstuff(*buf, 0);
-        Self::read_from(&cobs)
     }
 }
 
