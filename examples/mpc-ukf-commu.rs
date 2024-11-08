7e41a20c3ce5e7796e163a5a5fd351b9c93cac76
teruyamato0731 <teruyamato0731@gmail.com>
2024-11-08 05:38:05 +0000

    style: 定数を整理

--- examples/mpc-ukf-commu.rs
+++ examples/mpc-ukf-commu.rs
@@ -10,6 +10,8 @@ use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
+// MARK: - Constants
+
 // 予測ホライゾン
 const T: f64 = 0.8;
 const N: usize = 8;
@@ -19,6 +21,15 @@ const DT: f64 = T / N as f64;
 const LIMIT: (f64, f64) = (-10.0, 10.0);
 
 // UKF
+const PHY: f64 = 0.25;
+const Q: na::SMatrix<f64, 6, 6> = matrix![
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0;
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0;
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0;
+    0.0, 0.0, 0.0, 0.0, 0.0, 1.0;
+    0.0, 0.0, 0.0, 0.0, 1.0, 1e2;
+    0.0, 0.0, 0.0, 1.0, 1e2, 1e4;
+];
 const R: na::SVector<f64, 5> = vector![50.0, 50.0, 50.0, 0.2, 0.2];
 
 // MARK: - Main
@@ -180,16 +191,8 @@ fn gen_ref(x: &na::Vector4<f64>) -> na::SMatrix<f64, 4, N> {
 // MARK: - UKF
 fn init_ukf(init: &na::Vector6<f64>) -> Arc<Mutex<UnscentedKalmanFilter>> {
     let p = na::SMatrix::<f64, 6, 6>::identity() * 10.0;
-    let q = matrix![
-        0.0, 0.0, 0.0, 0.0, 0.0, 0.0;
-        0.0, 0.0, 0.0, 0.0, 0.0, 0.0;
-        0.0, 0.0, 0.0, 0.0, 0.0, 0.0;
-        0.0, 0.0, 0.0, 0.0, 0.0, 1.0;
-        0.0, 0.0, 0.0, 0.0, 1.0, 1e2;
-        0.0, 0.0, 0.0, 1.0, 1e2, 1e4;
-    ] * 0.25;
     let r = na::SMatrix::<f64, 5, 5>::from_diagonal(&R);
-    let obj = UnscentedKalmanFilter::new(*init, p, q, r);
+    let obj = UnscentedKalmanFilter::new(*init, p, PHY * Q, r);
     Arc::new(Mutex::new(obj))
 }
 fn hx(state: &na::Vector6<f64>) -> na::Vector5<f64> {
